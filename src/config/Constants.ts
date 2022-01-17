f7257e67c2b1a5c7e4ea10782ec79a15f84f02a7
dfilastro <d.filastro@hotmail.com>
2022-01-17 14:04:09 -0300

    Final version

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+  API_URL: 'http://localhost:5000',
+  MONGO_CONECTION:
+    'mongodb+srv://dfilastro:<password>@url-shortener-dio.zo9ts.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+};
